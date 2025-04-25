f3b65104d59153fa04ff4fd6be4fc70db0af4d10
Ilgiz Satkynov <satkynovilgiz008@gmail.com>
2025-04-25 17:14:27 +0600

    i create new CSS

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,52 +1,50 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [toDo, setTodo]=useState("")
-  const [task, setTask]=useState([])
+  const [toDo, setTodo] = useState('');
+  const [task, setTask] = useState([]);
 
-  function addTask(){
-    if(toDo==='') return;
-    setTask([...task, toDo]);
-    setTodo("")
+  function addTask() {
+    if (toDo.trim() === '') return; 
+    setTask(prevTasks => [...prevTasks, toDo]);
+    setTodo('');
   }
-function removeTask(index){
-  setTask(task.filter((_,i)=> i !== index))
-} 
+
+  function removeTask(index) {
+    setTask(prevTasks => prevTasks.filter((_, i) => i !== index));
+  }
+
   return (
-    <>
     <div className="ToDo">
       <h1>To-Do List with React</h1>
 
+      <input
+        type="text"
+        value={toDo}
+        placeholder="Write a new task here"
+        onChange={(e) => setTodo(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') addTask();
+        }}
+      />
+      <button onClick={addTask}>ADD</button>
 
-<input type="text" value={toDo} placeholder='write here new text'
-onChange={(e)=>{
-  setTodo(e.target.value)
-}}
-onKeyDown={(e)=>{
-  if (e.key === "Enter")
-    addTask()
-}}
-/>
-<button onClick={addTask}> ADD</button>
-<ul >
-  {
-    task.map(( item,index,)=>(
-      <div style={{display:'flex', alignItems:"center"}}>
-
-        <li key={index}>{item}</li>
-        <button onClick={()=>removeTask(index)}>
-          X
-        </button>
-      </div>
-    ))
-    
-  }
-</ul>
+      <ul>
+        {task.map((item, index) => (
+          <li key={index} className="task-item">
+            <span>{item}</span>
+            <button
+              className="remove-btn"
+              onClick={() => removeTask(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
